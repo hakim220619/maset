bf58c0b3f412a52f51575116270e963867681c9f
Dani lukman <danilukman2206@gmail.com>
2024-06-07 18:25:52 +0700

    logo login

--- resources/views/content/authentications/auth-login.blade.php
+++ resources/views/content/authentications/auth-login.blade.php
@@ -36,7 +36,8 @@
                         <!-- Logo -->
                         <div class="app-brand justify-content-center mb-4 mt-2">
                             <a href="{{ url('/') }}" class="app-brand-link gap-2">
-                                <span class="app-brand-logo demo">@include('_partials.macros', ['height' => 20, 'withbg' => 'fill: #fff;'])</span>
+                                <img src="{{ asset('') }}storage/images/logo/{{ Helper::aplikasi()->logo }}"
+                                    alt="" width="72" height="52">
                                 {{-- <span
                                     class="app-brand-text demo text-body fw-bold ms-1">{{ config('variables.templateName') }}</span> --}}
                             </a>
